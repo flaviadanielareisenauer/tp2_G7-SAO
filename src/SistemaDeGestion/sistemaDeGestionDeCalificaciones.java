f56de5219e7090731f396bfc60f81bb7ed837ba7
flaviadanielareisenauer <flaviareisenauer@gmail.com>
2023-11-26 12:05:11 -0300

    mensaje de bienvenida

--- src/SistemaDeGestion/sistemaDeGestionDeCalificaciones.java
+++ src/SistemaDeGestion/sistemaDeGestionDeCalificaciones.java
@@ -3,23 +3,40 @@ package SistemaDeGestion;
 import java.util.Scanner;
 
 public class SistemaDeGestionDeCalificaciones {
-    
-    static Scanner scanner = new Scanner(System.in);
-    
-    // variables
-    int cantidadDeAsignaturas = scanner.nextInt();
-    
-   
 
     public static void main(String[] args) {
+        // ENTRADA 
+        Scanner scanner = new Scanner(System.in);
+        
+        // VARIABLES
+        int totalAsignaturas = 0;
+        
+        // CONSTANTES
+        final int MAX_ASIGNATURAS = 10;
+        
+        // VECTORES
+        String[] asignaturas = new String[MAX_ASIGNATURAS];
+        
+        // MATRIZ     
+        
+                
         // Mensaje de bienvenida
-        System.out.println("*||*Bienvenido al Sistema de Gestion de Calificaciones*||*");
-        System.out.println("Para continuar presione Enter...");
+        System.out.println(
+                "************************************************************\n"
+              + "<<<<<BIENVENIDO AL SISTEMA DE GESTION DE CALIFICACIONES>>>>>\n"
+              + "************************************************************\n"
+        );
+        
+        // Enter para continuar...
+        System.out.println("Por favor, presione Enter para continuar...");
         scanner.nextLine();
         
-        // cantidad de asignaturas
-        System.out.println("Ingrese la cantidad de asignaturas: ");
+        // Ingresar la cantiad de asignaturas
+        System.out.println("Por favor, ingresa la cantidad de asignaturas. Las mismas no pueden superar un maximo de 10: ");
+        totalAsignaturas = scanner.nextInt();    
         
         
+
     }
-    
+
+}
