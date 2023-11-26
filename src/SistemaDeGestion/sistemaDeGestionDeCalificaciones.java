1dbc14802b9f524236b4d00f83462ae108dad2c1
flaviadanielareisenauer <flaviareisenauer@gmail.com>
2023-11-25 23:48:28 -0300

    validacion del numero de asignaturas

--- src/SistemaDeGestion/sistemaDeGestionDeCalificaciones.java
+++ src/SistemaDeGestion/sistemaDeGestionDeCalificaciones.java
@@ -5,11 +5,22 @@ import java.util.Scanner;
 public class sistemaDeGestionDeCalificaciones {
 
     public static void main(String[] args) {
-        System.out.println("|*|Bienvenido al Sistema de Gestion de Calificaciones|*|");
+        System.out.println("|*|Bienvenido al Sistema de Gestion de Calificaciones|*|"); 
         
         Scanner scanner = new Scanner(System.in);
         System.out.println("Presione Enter para poder continuar...");
         scanner.nextLine();
+        
+        System.out.println("Ingrese las asignaturas");
+        System.out.println("Ingrese el numero de asignaturas: ");
+        int cantidadDeAsignaturas = scanner.nextInt();
+        
+        // Valida que la cantidad de asignaturas no sea mayor de 10
+        if (cantidadDeAsignaturas < 1 || cantidadDeAsignaturas > 10) {
+            System.out.println("ERROR: el numero de asignatura a ingresar debe estar entre 1 y 10.");
+            return; // Sale del programa en caso de haber un error       
+            
+        }     
 
     }
     
