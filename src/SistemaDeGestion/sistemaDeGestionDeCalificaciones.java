7ca2a072b81916cc1fd4e0b107640ec0875db3fb
flaviadanielareisenauer <flaviareisenauer@gmail.com>
2023-11-25 23:17:11 -0300

    mensaje de bienvenida

--- src/SistemaDeGestion/sistemaDeGestionDeCalificaciones.java
+++ src/SistemaDeGestion/sistemaDeGestionDeCalificaciones.java
@@ -1,8 +1,15 @@
 package SistemaDeGestion;
 
+import java.util.Scanner;
+
 public class sistemaDeGestionDeCalificaciones {
 
     public static void main(String[] args) {
+        System.out.println("|*|Bienvenido al Sistema de Gestion de Calificaciones|*|");
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Presione Enter para poder continuar...");
+        scanner.nextLine();
 
     }
     
