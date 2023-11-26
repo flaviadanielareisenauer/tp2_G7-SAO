233fedb7f2ece76ea47b71abfe12851bc5f4c6f6
flaviadanielareisenauer <flaviareisenauer@gmail.com>
2023-11-26 14:41:17 -0300

    validacion de la cantidad, ingreso de materias y listado final

--- src/SistemaDeGestion/sistemaDeGestionDeCalificaciones.java
+++ src/SistemaDeGestion/sistemaDeGestionDeCalificaciones.java
@@ -10,48 +10,53 @@ public class SistemaDeGestionDeCalificaciones {
 
         // VARIABLES
         int totalAsignaturas = 0;
-
-        // CONSTANTES
-        final int MAX_ASIGNATURAS = 10;
+	int totalEstudiantes = 0;
 
         // VECTORES
-        String[] asignaturas = new String[MAX_ASIGNATURAS];
+        String[] asignaturas = new String[10];
+	String[] estudiantes = new String[30];
 
         // MATRIZ     
+        
+        
+
         // Mensaje de bienvenida
         System.out.println(
-                  "************************************************************\n"
-                + "<<<<<BIENVENIDO AL SISTEMA DE GESTION DE CALIFICACIONES>>>>>\n"
-                + "************************************************************\n"
+                "************************************************************\n"
+              + "<<<<<BIENVENIDO AL SISTEMA DE GESTION DE CALIFICACIONES>>>>>\n"
+              + "************************************************************\n"
         );
 
         // Enter para continuar...
         System.out.println("Por favor, presione Enter para continuar...");
         scanner.nextLine();
 
-        // Ingresar la cantiad de asignaturas
-        System.out.println("Por favor, ingrese la cantidad de asignaturas. Las mismas no pueden superar un maximo de 10: ");
+        System.out.println("Por favor, ingrese la cantidad de asignaturas. No deben superar el maximo de 10: ");
         totalAsignaturas = scanner.nextInt();
-
+        
+        
+	
+	    // Valida la cantidad de asignaturas que se ingresaron
+        if (totalAsignaturas < 1 || totalAsignaturas > 10) {
+            System.out.println("ERROR: el numero de asignaturas debe ser entre 1 y 10.");
+            return; // cierra el programa si hay un error
+        }   
+	
+        // Ingresar cada materia
         for (int j = 0; j < totalAsignaturas; j++) {
             System.out.println("Ingrese el nombre de la asignatura " + (j + 1) + ": ");
             asignaturas[j] = scanner.next();
         }
-
-        System.out.println("========================================================\n"
-                         + "========================================================\n");
-        // Asignaturas que se ingresaron
-        System.out.println("Asignaturas ingresadas: ");
+	
+        System.out.println("==================================================================\n"
+                         + "==================================================================\n");
+        
+	    // Listado de asignaturas ingresadas
+        System.out.println("**ASIGNATURAS INGRESADAS: **");
         for (String asignatura : asignaturas) {
             if (asignatura != null) {
                 System.out.println(asignatura);
             }
-
         }
-
-        System.out.println("========================================================\n"
-                         + "========================================================\n");
-
-    }
-
-}
+        System.out.println("==================================================================\n"
+                         + "==================================================================\n");
